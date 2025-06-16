ea37b52f47fb86a4e45d4752f2ec6578898db7fa
markjhku <mark.jh.ku@gmail.com>
2025-06-15 20:52:45 -0400

    Add files via upload

--- tests/test.py
+++ tests/test.py
@@ -5,46 +5,51 @@
 
 Unit test using simulated Gaussian response
 """
-
+import unittest
 from laser_calibration.ion_response_simulation import GaussianIonResponseSimulation
 from laser_calibration.laser_calibration_system import LaserCalibrationSystem
 
 from laser_calibration.grid_sweep_optimize import grid_sweep_optimize
 
+class LaserCalibrationTest(unittest.TestCase):
+    
+    def test_sim(self):
+        # parameters:
+        x_center = 0.1
+        y_center = 0.2
+        
+        # simulated response to be used
+        sim = GaussianIonResponseSimulation(
+            photon_number=100,
+            x_center=x_center,
+            y_center=y_center,
+            x_width=0.3,
+            y_width=0.4
+            )
+        
+        # instantiate a LaserCalibrationSystem class    
+        syst = LaserCalibrationSystem(
+            ion_response_function=sim.measure_ion_response
+            )
+    
+        # add mirrors to the LaserCalibrationSystem object
+        syst.add_mirror("x", None)
+        syst.add_mirror("y", None)    
+        
+        # the following two lines are needed for simulation mode
+        syst.simulation = True
+        syst.simulation_mirror_set = ["x","y"]
+        
+        # perform optimization of ion response to calibrate the system
+        output = grid_sweep_optimize(laser_syst = syst,plot=False)
+        
+        # Check operating points are found within tolerance
+        x_result_withitn_tolerance =  abs(output['x'] - x_center) < 0.05 
+        y_result_withitn_tolerance = abs(output['y'] - y_center) < 0.05
+        
+        self.assertTrue(x_result_withitn_tolerance and y_result_withitn_tolerance)
+
+
 if __name__ == "__main__":
     
-    # parameters:
-    x_center = 0.1
-    y_center = 0.2
-    
-    # simulated response to be used
-    sim = GaussianIonResponseSimulation(
-        photon_number=100,
-        x_center=x_center,
-        y_center=y_center,
-        x_width=0.3,
-        y_width=0.4
-        )
-    
-    # instantiate a LaserCalibrationSystem class    
-    syst = LaserCalibrationSystem(
-        ion_response_function=sim.measure_ion_response
-        )
-
-    # add mirrors to the LaserCalibrationSystem object
-    syst.add_mirror("x", None)
-    syst.add_mirror("y", None)    
-    
-    # the following two lines are needed for simulation mode
-    syst.simulation = True
-    syst.simulation_mirror_set = ["x","y"]
-    
-    # perform optimization of ion response to calibrate the system
-    output = grid_sweep_optimize(laser_syst = syst,plot=False)
-    
-    # Check operating points are found within tolerance
-    if abs(output['x'] - x_center) < 0.05 and abs(output['y'] - y_center) < 0.05:
-        print("test passed")
-
-    else:
-        print("test failed")
+    unittest.main()
